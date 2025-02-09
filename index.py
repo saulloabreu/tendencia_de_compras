18fd927cd2ef749f95345c4179542bd516e1daca
saulloabreu <mts2204.abreu@gmail.com>
2025-02-09 05:37:29 -0300

    atualização de script -correção erro

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import dash
 from dash import html, dcc, Input, Output, State
 import dash_bootstrap_components as dbc
+import plotly
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
